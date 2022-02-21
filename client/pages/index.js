18e81c90c0df9bac7c1c2094ff14ebd33e1cb97c
bevanvarghese <bevanvar@connect.hku.hk>
2022-02-21 21:00:43 +0800

    # frontend & schema setup

---
+++ client/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Main from '../components/Main'
+
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
+}
+
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+      <Header />
+      <Main />
+      <h2>Transaction History</h2>
+    </div>
+  )
+}
